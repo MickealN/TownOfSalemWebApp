d093ac088b376bc1503cde74b0041ee99ba97a16
Mickeal Nevakshonoff <c3cvlsj4@ca.ibm.com>
2017-11-25 05:25:02 -0600

    Pushing changes from the work I did tonight

--- js/client.js
+++ js/client.js
@@ -1,6 +1,15 @@
 var Client = {};
 Client.socket = io.connect();
+var playerRole;
+var myId;
+var totalPlayers = 0;
+var alreadyVotedFor = -1;
+var currentTable;
 
+
+//Grabs the inputted information from the webpage and submits it to the server
+//Entering "mod" into playername will display the game moderator controls.
+//This has not been made a button as to prevent THOSE DINKS FROM MESSING WITH MY CONTROL OF THE GAME
 Client.askNewPlayer = function(){
 	var name = document.getElementById("playerName");
 	var role = document.getElementById("playerRole");
@@ -9,14 +18,41 @@ Client.askNewPlayer = function(){
 		alert("Submit a name you twit!");
 		return false;
 	} else if(name.value=="mod"){
-		alert("Mod tools exposed");
+		//alert("Mod tools exposed");
 		document.getElementById("modtools").style.display = "inline";
 		return false;
 	}
+	playerRole = role.value;
     Client.socket.emit('newplayer', {playerName:name.value,
 		playerRole:role.value});
 };
 
+Client.targetPlayer = function(aggroId, victimId){
+	Client.socket.emit('targetplayer', aggroId, victimId);
+};
+
+Client.voteAgainst = function(voterId, victimId){
+	if(alreadyVotedFor == -1){
+		Client.socket.emit('voteagainst', voterId, victimId, 1);
+		Client.socket.emit('messagebroadcast', currentTable[voterId].name + "  voted against "
+		+ currentTable[victimId].name+"!");
+		alreadyVotedFor = victimId;
+	} else if(alreadyVotedFor == victimId) {
+		Client.socket.emit('voteagainst', voterId, alreadyVotedFor, -1);
+		Client.socket.emit('messagebroadcast', currentTable[voterId].name + " cancelled their vote!");
+		alreadyVotedFor = -1;
+	} else {
+		Client.socket.emit('voteagainst', voterId, alreadyVotedFor, -1);
+		Client.socket.emit('voteagainst', voterId, victimId, 1);
+		Client.socket.emit('messagebroadcast', currentTable[voterId].name + "  changed their vote to "
+		+ currentTable[victimId].name+"!");
+		alreadyVotedFor = victimId;
+	}
+};
+
+
+
+//Moderator Controls. Self explanitory.
 Client.startGame = function(){
 	Client.socket.emit('startgame');
 };
@@ -28,12 +64,33 @@ Client.startDay = function(){
 Client.startNight = function(){
 	Client.socket.emit('startnight');
 };
+Client.report = function(){
+	Client.socket.emit('report');
+};
 
+
+
+
+
+
+
+//Recieves a new player from the server and adds it to the list on screen.
+//Input: Player object {id, role, name, alive:boolean}
 Client.socket.on('newplayer', function(data){
+	totalPlayers++;
 	document.getElementById("playerList").innerHTML += data.name;
 });
 
-Client.socket.on('allplayers', function(data){
+//Get's a list of all connected players and hides the start screen from the connected user.
+//Input: playerList Array
+//		{id, role, name, alive:boolean}
+Client.socket.on('allplayers', function(data, id){
+	
+	//This is a hack and you know it Mickeal
+	if(typeof id != "undefined" ){
+		myId = id;
+	}
+	totalPlayers = data.length;
 	document.getElementById("playerList").innerHTML="";
 	for(i = 0; i < data.length; i++){
 		document.getElementById("playerList").innerHTML += data[i].name + "<br>";
@@ -41,12 +98,62 @@ Client.socket.on('allplayers', function(data){
 	document.getElementById("startScreen").style.display = "none";
 });
 
+//A function to notify the user that they cannot join a game in progress
 Client.socket.on('gamealreadystarted', function(data){
 	alert("Game has already started!");
 	//Set up observer tools later.
 });
 
-Client.socket.on('gamestart', function(data){
+//Edit the screen to show the live players and the night action table
+//Notes: That tabletext line is so hacky is makes me uncomfortable
+//Input: playerList Array
+//		{id, role, name, alive:boolean}
+Client.socket.on('sendnighttable', function(data){
 	
-	
+	var tableText = "<table>";
+	for(i = 0; i < totalPlayers; i++){
+		if(data[i].alive){
+			tableText += "<tr><td>" + data[i].name +  
+			"</td><td><button onclick=\"Client.targetPlayer("+ myId + ","+ data[i].id + 
+			")\">Target</button></td></tr>";
+		} else if(data[i].alive){
+			tableText += "<tr><td>" + data[i].name +  "</td><td>Alive</td></tr>";
+		} else {
+			tableText += "<tr><td>" + data[i].name +  "</td><td>DEAD</td></tr>";
+		}
+	}
+	tableText += "</table>";
+	document.getElementById("nightTable").innerHTML = tableText;
+	document.getElementById("nightTable").style.display = "inline";
+	document.getElementById("dayTable").style.display = "none";
+});
+
+//Edit the screen to show the live players and the day action table
+//Notes: That tabletext line is so hacky is makes me uncomfortable
+//Input: playerList Array
+//		{id, role, name, alive:boolean}
+Client.socket.on('senddaytable', function(data){
+	currentTable = data;
+	alreadyVotedFor = -1;
+	document.getElementById("playerList").style.display = "none";
+	var tableText = "<table>";
+	for(i = 0; i < totalPlayers; i++){
+		if(data[i].alive && data[myId].alive){
+			tableText += "<tr><td>" + data[i].name +  
+			"</td><td><button onclick=\"Client.voteAgainst("+ myId + ","+ data[i].id + 
+			")\">Vote</button></td></tr>";
+		} else if(data[i].alive){
+			tableText += "<tr><td>" + data[i].name +  "</td><td>Alive</td></tr>";
+		} else {
+			tableText += "<tr><td>" + data[i].name +  "</td><td>DEAD</td></tr>";
+		}
+	}
+	tableText += "</table>";
+	document.getElementById("dayTable").innerHTML = tableText;
+	document.getElementById("dayTable").style.display = "inline";
+	document.getElementById("nightTable").style.display = "none";
+});
+
+Client.socket.on('messagebroadcast', function(message){
+	document.getElementById("messageField").innerHTML += "<br>" + message;
 });
--- server.js
+++ server.js
@@ -4,6 +4,13 @@ var server = require('http').Server(app);
 var io = require('socket.io').listen(server);
 
 var gameRunning = false;
+var isNight = false; // True is night, false is day
+var dayCounter = 0;
+var tArray = []; //Targetting Array
+var vArray = [];
+var voteThreshhold;
+
+
 //app.use('/css',express.static(__dirname + '/css'));
 app.use('/js',express.static(__dirname + '/js'));
 //app.use('/assets',express.static(__dirname + '/assets'));
@@ -23,16 +30,113 @@ server.playerList = [];
 
 io.on('connection', function(socket){
 	
-	//The disconect code is buggy right now.
+	//Checks if the player has an ID in the server, and if they do, marks them as dead
+	//Otherwise, does nothing
 	socket.on('disconnect', function(){
+		if(typeof socket.player === "undefined"){
+			console.log('Disconnecting a player who didnt have an ID');
+			return false;
+		}
+		
 		server.playerList[socket.player.id].alive = false;
 		console.log('Disconnecting player ' + socket.player.id);
 		io.emit('allplayers', server.playerList);
 	});
 	
+	
+	//Game controls, self explanitory
 	socket.on('startgame', function(){
 		gameRunning = true;
 	});
+	socket.on('startday', function(){
+		if(!gameRunning){
+			return false;
+		}
+		io.emit('senddaytable', server.playerList);
+		dayCounter++;
+	});
+	socket.on('startnight', function(){
+		if(!gameRunning){
+			return false;
+		}
+		io.emit('sendnighttable', server.playerList);
+	});
+	
+	socket.on('report', function(){
+		if(!gameRunning){
+			return false;
+		}
+		console.log(tArray);
+		//Process results of day/night
+		
+		
+		
+		for(i = 0; i < tArray.length; i++){
+			if(typeof tArray[i] != "undefined" ){
+				switch(server.playerList[i].role){
+					case "werewolf":
+						server.playerList[tArray[i]].alive = false;
+						server.playerList[tArray[i]].deathTurn = dayCounter;
+						server.playerList[tArray[i]].deathCause = "werewolf";
+						voteThreshhold--;
+						break;
+					case "townie":
+						break;
+				}
+			
+			}
+		}
+		
+		for(i = 0; i < server.playerList.length; i++){
+			
+			if(server.playerList[i].deathTurn == dayCounter){
+				console.log(server.playerList[i].name + " died to a " + 
+				server.playerList[i].deathCause + " tonight!");
+			}
+			
+		}
+		
+		
+		//Clear the targetting table for next voting cycle
+		tArray = [];//[server.playerList.length];
+	});
+	
+	
+	socket.on('targetplayer', function(aggroId, victimId){
+		
+		if(tArray[aggroId] == victimId){
+			delete tArray[aggroId];
+			console.log(server.playerList[aggroId].name + ' decided to stay home tonight.');
+		} else {
+			tArray[aggroId] = victimId;
+			console.log(server.playerList[aggroId].name + ' targeted ' + server.playerList[victimId].name);
+		}
+	});
+	
+	socket.on('voteagainst', function(voterId, victimId, increment){
+		
+		if(isNaN(vArray[victimId])){ //initialize the element
+			vArray[victimId] = 1;
+		} else {
+			vArray[victimId] += increment;
+		}
+
+		if(vArray[victimId] >= voteThreshhold){
+			console.log(server.playerList[victimId].name + ' is now on trial.');
+			//TODO: Include code that puts the voted on trial
+		}
+		
+	});
+	
+	socket.on('messagebroadcast', function(message){
+		io.emit('messagebroadcast', message);
+	});
+	
+	
+	
+	
+	
+	
 	
     socket.on('newplayer', function(data){
         if(!gameRunning){
@@ -40,15 +144,18 @@ io.on('connection', function(socket){
 	            id: server.lastPlayerID++,
 	            role: data.playerRole,
 				name: data.playerName,
-				alive: true
+				alive: true,
+				deathCause: "",
+				deathTurn: -1				//-1 means alive
 			
 	        };
 			server.playerList.push(socket.player);
-			console.log(server.playerList);
+			//console.log(server.playerList);
 			//console.log('New Player: 	Role-' + socket.player.role + '\n		Name-' + 
 				//socket.player.name + '\n		playerID-' + socket.player.id);
-			socket.emit('allplayers', server.playerList);
+			socket.emit('allplayers', server.playerList, socket.player.id);
 	        socket.broadcast.emit('newplayer', socket.player);
+			voteThreshhold = (server.playerList.length/2) + 0.5;
 		} else {
 			socket.emit('gamealreadystarted');
 		}
